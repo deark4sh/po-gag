9878edfe070fa0dea87773672bc900a5f8112724
deark4sh <jsonsrd@gmail.com>
2022-10-26 20:23:18 -0300

    Otimização no código.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time, random as r, string as st
+import random as r, string as st
 
 senha=""
-for x in range(13): senha=senha+r.choice(st.ascii_letters+st.digits+st.punctuation)
+for _ in range(13): senha=senha+r.choice(st.ascii_letters+st.digits+st.punctuation)
 print(f"Sua nova senha é: {senha}")
